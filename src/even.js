982f360bf12248053855b861199269fd5179d9ea
Denis Voinovich <denzelvojn@gmail.com>
2024-01-04 20:00:35 +0300

    refactor even function

--- src/even.js
+++ src/even.js
@@ -9,21 +9,14 @@ const evenFunc = (randomNum, name) => {
   while (loop) {
     const num = randomNum();
     const answer = readlineSync.question(`Question: ${num} \n`);
-    if (num % 2 === 0 && answer === 'yes') {
+    if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
       count += 1;
       if (count === 3) {
         count = 0;
         console.log(`Congratulations, ${name}!`);
       }
-    } else if (num % 2 !== 0 && answer === 'no') {
-      console.log('Correct!');
-      count += 1;
-      if (count === 3) {
-        count = 0;
-        console.log(`Congratulations, ${name}!`);
-      }
-    } else if (num % 2 === 0 && answer === 'no') {
+    } else if ((num % 2 === 0 && answer === 'no') || (num % 2 !== 0 && answer === 'yes') || (answer !== 'yes' || answer !== 'no')) {
       count = 0;
       if (answer === 'yes') {
         console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
@@ -32,17 +25,6 @@ const evenFunc = (randomNum, name) => {
       }
       console.log(`Let's try again, ${name}!`);
       loop = false;
-    } else if (num % 2 !== 0 && answer === 'yes') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${name}!`);
-      loop = false;
-    } else if (answer !== 'yes' || answer !== 'no') {
-      count = 0;
-      if (answer === 'yes') {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
-      } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      }
     }
   }
 };
