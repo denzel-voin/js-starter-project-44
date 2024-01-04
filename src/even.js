129834b085bd7e68f5004e50ebfd0db530496414
Denis Voinovich <denzelvojn@gmail.com>
2024-01-04 20:49:45 +0300

    refactor even function

--- src/even.js
+++ src/even.js
@@ -3,29 +3,26 @@ import readlineSync from 'readline-sync';
 const random = () => Math.floor(Math.random() * 100 + 1);
 
 const evenFunc = (randomNum, name) => {
-  let loop = true;
   let count = 0;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  while (loop) {
+  while (true) {
     const num = randomNum();
     const answer = readlineSync.question(`Question: ${num} \n`);
     if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
-      count += 1;
-      if (count === 3) {
+      if (++count === 3) {
         count = 0;
         console.log(`Congratulations, ${name}!`);
       }
     } else {
       count = 0;
-      if (answer === 'yes') console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
-      else if (answer === 'no') console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      else if (num % 2 === 0) console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      else console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+      const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      loop = false;
+      break
     }
   }
 };
+
 
 export { random, evenFunc };
